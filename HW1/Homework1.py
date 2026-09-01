05b5a47e85a5620b7df22aab9ce0e68624c591e4
Ahmed Khan <ahmedk2@illinois.edu>
2026-08-31 22:24:36 -0500

    Just need to write written up for Hw1

---
+++ HW1/Homework1.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt 
+import scipy.integrate as sp 
+
+R = 6378 # km
+mu = 398600 # km^3/s^2
+time = 3 * 60 * 60 # 3 hours -> seconds
+
+def main():
+    r_0 = np.array([-307.8439, -4517.4816, -5747.6257]) # km
+    v_0 = np.array([5.1593, 3.795, -3.2591]) #km
+    state_0 = np.array([*v_0, *r_0]) # initial state
+    
+    sol  = sp.solve_ivp(ODE, (0, time), state_0, rtol = 1e-13) # solving the ODE 
+    time_pts = sol['t'] # time points 
+    state_vects = sol['y'] # state vectors 
+
+    velocity_vecs = state_vects[:3].T # velocity vecs
+    position_vecs = state_vects[3:].T # position vecs 
+
+    angular_vel_vecs = [] 
+    h_t0 = np.linalg.cross(position_vecs[0], velocity_vecs[0])
+    for vel_vec, pos_vec in zip(velocity_vecs, position_vecs): 
+        h_t = np.linalg.cross(pos_vec,vel_vec) 
+        angular_vel_vecs.append(np.linalg.norm(h_t - h_t0)/np.linalg.norm(h_t0))
+
+
+        
+    x, y, z = position_vecs.T[0], position_vecs.T[1], position_vecs.T[2]
+    fig = plt.figure() 
+    ax = fig.add_subplot(111, projection ="3d")
+    ax.plot(x, y, z)
+    ax.set_title("Sputnik Orbit")
+    ax.set_xlabel("X (km)")
+    ax.set_ylabel("Y (km)")
+    ax.set_zlabel("Z (km)")
+
+    fig2 = plt.figure() 
+    ax2 = fig2.add_subplot(111)
+    ax2.plot(time_pts, angular_vel_vecs)
+    ax2.set_title("Specific Angular Momentum Conservation Error v Time")
+    ax2.set_xlabel("Time (s)")
+    ax2.set_ylabel("Specific Angular Momentum Conservation Error")
+    ax2.set_yscale("log")
+
+
+    plt.show()
+
+    
+
+    
+
+
+
+def ODE(time, state):
+    v = state[:3]
+    r = state[3:]
+    r_dd = -mu*r/(np.linalg.norm(r)**3)
+    r_d = v 
+    return np.array([*r_dd, *r_d]) 
+
+
+# https://chatgpt.com/share/6a9607e9-fbe0-83ea-9a53-09eb333a7289
+main() 
