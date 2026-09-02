98b12e12cec1e777cebe0a9b956446ccda3266a3
Ahmed Khan <ahmedk2@illinois.edu>
2026-09-01 22:40:24 -0500

    Code

--- HW1/Homework1.py
+++ HW1/Homework1.py
@@ -8,12 +8,13 @@ mu = 398600 # km^3/s^2
 time = 3 * 60 * 60 # 3 hours -> seconds
 
 def main():
+    # initial conditions 
     r_0 = np.array([-307.8439, -4517.4816, -5747.6257]) # km
     v_0 = np.array([5.1593, 3.795, -3.2591]) #km
     state_0 = np.array([*v_0, *r_0]) # initial state
 
-
-    sol  = sp.solve_ivp(ODE, (0, time), state_0, rtol = 1e-13) # solving the ODE 
+    # numerical integration 
+    sol  = sp.solve_ivp(ODE, (0, time), state_0, method='RK45', rtol = 1e-13, atol = 1e-13) # solving the ODE 
     time_pts = sol['t'] # time points 
     state_vects = sol['y'] # state vectors 
 
@@ -21,13 +22,13 @@ def main():
     position_vecs = state_vects[3:].T # position vecs 
 
     angular_vel_vecs = [] 
-    h_t0 = np.linalg.cross(position_vecs[0], velocity_vecs[0])
+    h_t0 = np.linalg.cross(position_vecs[0], velocity_vecs[0]) # initial specific angular momentum 
     for vel_vec, pos_vec in zip(velocity_vecs, position_vecs): 
-        h_t = np.linalg.cross(pos_vec,vel_vec) 
-        angular_vel_vecs.append(np.linalg.norm(h_t - h_t0)/np.linalg.norm(h_t0))
+        h_t = np.linalg.cross(pos_vec,vel_vec)   # angular momentum 
+        angular_vel_vecs.append(np.linalg.norm(h_t - h_t0)/np.linalg.norm(h_t0)) # conservation error
 
 
-    sol2  = sp.solve_ivp(ODE, (0, time), state_0, rtol = 1e-7) # solving the ODE 
+    sol2  = sp.solve_ivp(ODE, (0, time), state_0,method='RK45', rtol = 1e-7, atol = 1e-7) # solving the ODE 
     time_pts2 = sol2['t'] # time points 
     state_vects2 = sol2['y'] # state vectors 
 
@@ -35,15 +36,14 @@ def main():
     position_vecs2 = state_vects2[3:].T # position vecs 
 
     angular_vel_vecs2 = [] 
-    h_t02 = np.linalg.cross(position_vecs2[0], velocity_vecs2[0])
+    h_t02 = np.linalg.cross(position_vecs2[0], velocity_vecs2[0]) # initial specific angular momentum 
     for vel_vec, pos_vec in zip(velocity_vecs2, position_vecs2): 
-        h_t2 = np.linalg.cross(pos_vec,vel_vec) 
-        angular_vel_vecs2.append(np.linalg.norm(h_t2 - h_t02)/np.linalg.norm(h_t02))
+        h_t2 = np.linalg.cross(pos_vec,vel_vec)  # angular momentum 
+        angular_vel_vecs2.append(np.linalg.norm(h_t2 - h_t02)/np.linalg.norm(h_t02)) # conservation error
 
-
-        
+    # Plotting the results
     x, y, z = position_vecs.T[0], position_vecs.T[1], position_vecs.T[2]
-    fig = plt.figure(dpi=100) 
+    fig = plt.figure(dpi=300) 
     ax = fig.add_subplot(111, projection ="3d")
     ax.plot(x, y, z)
     ax.set_title("Sputnik Orbit (tol = 1e-13)")
@@ -51,7 +51,7 @@ def main():
     ax.set_ylabel("Y (km)")
     ax.set_zlabel("Z (km)")
     
-    fig2 = plt.figure(dpi=100) 
+    fig2 = plt.figure(dpi=300) 
     ax2 = fig2.add_subplot(111)
     ax2.plot(time_pts, angular_vel_vecs)
     ax2.set_title("Specific Angular Momentum Conservation Error v Time (tol = 1e-13)")
@@ -60,7 +60,7 @@ def main():
     ax2.set_yscale("log")
 
     x2, y2, z2 = position_vecs2.T[0], position_vecs2.T[1], position_vecs2.T[2]
-    fig3 = plt.figure(dpi=100) 
+    fig3 = plt.figure(dpi=300) 
     ax3 = fig3.add_subplot(111, projection ="3d")
     ax3.plot(x2, y2, z2)
     ax3.set_title("Sputnik Orbit (tol = 1e-7)")
@@ -68,7 +68,7 @@ def main():
     ax3.set_ylabel("Y (km)")
     ax3.set_zlabel("Z (km)")
     
-    fig4 = plt.figure(dpi=100) 
+    fig4 = plt.figure(dpi=300) 
     ax4 = fig4.add_subplot(111)
     ax4.plot(time_pts2, angular_vel_vecs2)
     ax4.set_title("Specific Angular Momentum Conservation Error v Time (tol = 1e-7)")
@@ -76,10 +76,10 @@ def main():
     ax4.set_ylabel("Specific Angular Momentum Conservation Error")
     ax4.set_yscale("log")
 
-    fig.savefig("orbit_13")
-    fig2.savefig("conv_13")
-    fig3.savefig("orbit_7")
-    fig4.savefig("conv_7")
+    fig.savefig("./HW1/orbit_13")
+    fig2.savefig("./HW1/conv_13")
+    fig3.savefig("./HW1/orbit_7")
+    fig4.savefig("./HW1/conv_7")
     plt.show()
 
     
